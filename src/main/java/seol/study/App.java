185a2393e7cd7c8989f034cb2af8ef258dca29e1
seol <seolnavy@naver.com>
2020-05-05 20:38:25 +0900

    main메소드 수정

--- src/main/java/seol/study/App.java
+++ src/main/java/seol/study/App.java
@@ -1,13 +1,23 @@
 package seol.study;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import seol.study.cafe.barista.Barista;
+import seol.study.cafe.barista.BaristaImpl;
+import seol.study.cafe.customer.Customer;
+import seol.study.cafe.customer.CustomerImpl;
+import seol.study.cafe.drink.Drink;
+
+
+public class App {
+    public static void main( String[] args ) {
+//        final String menuName = "Americano";
+        final String menuName = "CafeLatte";
+        Customer customer = new CustomerImpl();
+        Barista barista = new BaristaImpl();
+
+        customer.order(barista, menuName);
+        Drink coffee = customer.getDrink();
+        System.out.println("coffee.getName() = " + coffee.getName());
+        System.out.println("coffee.getPrice() = " + coffee.getPrice());
+        System.out.println("coffee.getIngredient() = " + coffee.getIngredient());
     }
 }
